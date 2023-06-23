cda94563518854db047953ded545dcc095794128
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-06-23 10:22:09 +0700

    buat halaman notfound dan routenya

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home.page";
+import NotfoundPage from "./pages/notfound.page";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <HomePage /> } />
+        <Route path="*" element={ <NotfoundPage /> } />
       </Routes>
     </BrowserRouter>
   );
