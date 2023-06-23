b56dd016c9cef91d481e4039701eb0a8815a013b
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-06-23 11:05:24 +0700

    tambah component slider dan navbar

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import NavbarComponent from '../components/navbar.component';
+import CarouselComponent from '../components/carousel.component';
 
 const HomePage = () => {
   return (
     <>
-      <div className="container">
-        <h1>Hello World</h1>
+      <NavbarComponent />
+      <div className="container mt-3">
+        <CarouselComponent />
       </div>
     </>
   );
