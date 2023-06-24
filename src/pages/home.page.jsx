66440f6f1762f8595f1c849df4afe31c3e751d85
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-06-24 12:32:02 +0700

    tambah component kedalam halaman home (utama)

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import NavbarComponent from '../components/navbar.component';
 import CarouselComponent from '../components/carousel.component';
+import ListCategoryComponent from '../components/listCategory.component';
+import MustTryRecipeComponent from '../components/mustTryRecipe.component';
+import AreaRecipeComponent from '../components/areaRecipe.component';
+import MessageUpdateComponent from '../components/messageUpdate.component';
+import FooterComponent from '../components/footer.component';
 
 const HomePage = () => {
   return (
@@ -8,7 +13,12 @@ const HomePage = () => {
       <NavbarComponent />
       <div className="container mt-3">
         <CarouselComponent />
+        <ListCategoryComponent />
+        <MustTryRecipeComponent />
+        <AreaRecipeComponent />
+        <MessageUpdateComponent />
       </div>
+      <FooterComponent />
     </>
   );
 }
