77fc3237c2ee3cda6a2de6b307ef2b8a8a33c478
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-06-24 12:31:39 +0700

    tambah component footer

---
+++ src/components/footer.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FooterComponent = () => {
+  return (
+    <>
+      <div className="footer-component py-3" style={{ marginTop: '70px', backgroundColor: '#02A057' }}>
+        <h5 className='text-center' style={{ color: '#FFF', fontSize: '14px' }}>
+          Copyright © Masak Apa 2023
+        </h5>
+      </div>
+    </>
+  );
+}
+
+export default FooterComponent;
